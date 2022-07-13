06b55af4b661004c5eeade32056f6aa303de4893
Murad Ibragimov <texet0598@gmail.com>
2022-07-13 18:45:16 +0300

    fix

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,5 @@
 const acc_btn = document.querySelectorAll('.action-button');
+const cross_btn = document.querySelectorAll('.cross');
 
 acc_btn.forEach((el, i) => {
   el.addEventListener('click', () => {
@@ -9,5 +10,15 @@ acc_btn.forEach((el, i) => {
       panel.style.maxHeight = null: 
         panel.style.maxHeight = panel.scrollHeight + "px";
     el.classList.toggle('active');
+  })
+})
+cross_btn.forEach((el, i) => {
+  el.addEventListener('click', () => {
+    const panel = el.nextElementSibling.nextElementSibling;
+    el.classList.toggle('rotate')
+    panel.style.maxHeight ? 
+      panel.style.maxHeight = null: 
+        panel.style.maxHeight = panel.scrollHeight + "px";
+    el.nextElementSibling.classList.toggle('active');
   })
 })
